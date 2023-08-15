34bac3a1f3322d6465d71c948bb631c1e21c6b36
sorrychoe <cjssoote@gmail.com>
2023-08-15 17:02:17 +0900

    add 2 case

---
+++ tests/testthat/test_press_counter.R
@@ -0,0 +1,11 @@
+library(readxl)
+df <- read_xlsx("./test.xlsx")
+
+# header_remover 테스트
+test_that("press_counter가 언론사 갯수를 정확히 세는지 체크합니다.", {
+  result <- press_counter(df)
+  expect_equal(result$언론사[1], "경향신문")
+  expect_equal(result$기사[1], 6)
+})
+
+
